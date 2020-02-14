b494bc85aed9e807ede33c0bda54ff8ba89f7666
DrewU <dh961608@yahoo.com>
2020-02-14 12:41:47 -0800

    Sock matching almost done

--- HW3/HW3/HW3/Program.cs
+++ HW3/HW3/HW3/Program.cs
@@ -2,20 +2,21 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace HW3 {
     class SockMatching {
-        private static ConcurrentBag<Sock> bag = new ConcurrentBag<Sock>();
+        private static readonly ConcurrentBag<Sock> bag = new ConcurrentBag<Sock>();
 
         public static void Main(String[] args) {
             string[] colors = { "red", "blue", "green", "orange" };
             List<Task> tasks = new List<Task>();
-          
+
             foreach (var color in colors) {
                 Random random = new Random();
-                Task t = Task.Run(() => createSocks(color, random.Next(1, 100)));
+                Task t = Task.Run(() => createSocks(color, random.Next(1, 10)));
                 tasks.Add(t);
             }
             Task.WaitAll(tasks.ToArray());
@@ -28,50 +29,62 @@ namespace HW3 {
         }
 
         public static void createSocks(String color, int num) {
-            for(int i = 0; i < num; i++) {
+            for (int i = 0; i < num; i++) {
                 Sock sock = new Sock(color);
                 bag.Add(sock);
-                Console.WriteLine(sock.color + " sock is created!");
+                Console.WriteLine(color + " thread: Created a " + color + " sock");
             }
         }
 
-
-
         public static void matchSocks() {
-            Sock tmpSock = null;
-            bag.TryTake(out tmpSock);
-            int pairs = 0;
-            foreach(Sock sock in bag) {
-                if (sock.color.Equals(tmpSock.color)) {
-                    Boolean foundPair = false;
-                    int size = bag.Count;
+            List<Sock> sockList = bag.ToList();
+            int redCount = 0;
+            int blueCount = 0;
+            int greenCount = 0;
+            int orangeCount = 0;
 
-                    while ((foundPair == false)) {
-                        Sock tmp = null;
-                        bag.TryTake(out tmp);
-
-                        if (sock.color.Equals(tmp.color)) {
-                            foundPair = true;
-                        } else {
-                            bag.Add(tmp);
-                        }
-
-                        size--;
-                        //Console.WriteLine(pairs);
-                    }
-                    pairs++;
+            Dictionary<String, int> colorCounts = new Dictionary<String, int>();
+            foreach(Sock sock in sockList) {
+                switch(sock.color){
+                    case "red": redCount++;
+                        break;
+                    case "blue": blueCount++;
+                        break;
+                    case "green": greenCount++;
+                        break;
+                    case "orange": orangeCount++;
+                        break;
                 }
             }
 
-            Console.WriteLine(pairs);
-        }
-    }
+            colorCounts.Add("red", redCount);
+            colorCounts.Add("blue", blueCount);
+            colorCounts.Add("orange", orangeCount);
+            colorCounts.Add("green", greenCount);
 
-    class Sock {
-        public String color;
-        public Sock(String sockColor) {
-            color = sockColor;
+            foreach(var count in colorCounts) {
+                for(int i = 0; i < count.Value/2; i++) {
+                    sockList.Remove(new Sock(count.Key));
+                    sockList.Remove(new Sock(count.Key));
+                    Console.WriteLine("Matching thread: Found a pair of " + count.Key + " socks");
+                    // Needs to be a task queue
+                    new Thread(() => washingMachine(new Sock(count.Key), new Sock(count.Key))).Start();
+                }
+            }
         }
-    }
 
+        static void washingMachine(Sock sock1, Sock sock2) {
+            Console.WriteLine("Washer Thread: Destroyed " + sock1.color + " Socks");
+            sock1 = null;
+            sock2 = null;
+        }
+
+        class Sock{
+            public String color;
+            public Sock(String sockColor) {
+                color = sockColor;
+            }
+        }
+    
+    }
 }
