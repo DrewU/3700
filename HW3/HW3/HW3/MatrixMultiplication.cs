53726c137faf20f20e5c1c94d3248741004e05b5
Drew Umlang <dh961608@yahoo.com>
2020-02-17 17:30:37 -0800

    Update:

--- HW3/HW3/HW3/MatrixMultiplication.cs
+++ HW3/HW3/HW3/MatrixMultiplication.cs
@@ -3,27 +3,47 @@ using System.Threading;
 using System.Collections.Generic;
 
 namespace MatrixMultiplication {
+    class Program {
+        static void tmp(string[] args) {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m=");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("k=");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Matrix A = new Matrix(n, m).GenerateRandomMatrix();
+            Matrix B = new Matrix(m, k).GenerateRandomMatrix();
+            A.Print();
+            Console.WriteLine(new String('-', 20));
+            B.Print();
+            Console.WriteLine(new String('-', 20));
+            Matrix C = A * B;
+            C.Print();
+            Console.ReadLine();
+        }
+    }
+
     class Matrix {
-        public int Row { get; set; }
-        public int Column { get; set; }
+        public int rowSize;
+        public int columnSize;
         double[,] arr;
         public static Mutex mutex = new Mutex();
 
         Matrix() { }
         public Matrix(int row, int column) {
-            Row = row;
-            Column = column;
+            this.rowSize = row;
+            this.columnSize = column;
             arr = new double[row, column];
         }
         public double[] GetColumn(int i) {
-            double[] res = new double[Row];
-            for (int j = 0; j < Row; j++)
+            double[] res = new double[rowSize];
+            for (int j = 0; j < rowSize; j++)
                 res[j] = arr[j, i];
             return res;
         }
         public double[] GetRow(int i) {
-            double[] res = new double[Column];
-            for (int j = 0; j < Column; j++)
+            double[] res = new double[columnSize];
+            for (int j = 0; j < columnSize; j++)
                 res[j] = arr[i, j];
             return res;
         }
@@ -31,27 +51,27 @@ namespace MatrixMultiplication {
             get { return arr[i, j]; }
             set { arr[i, j] = value; }
         }
-        public Matrix RandomValues() {
+        public Matrix GenerateRandomMatrix() {
             Random rnd = new Random();
-            for (int i = 0; i < Row; i++)
-                for (int j = 0; j < Column; j++)
+            for (int i = 0; i < rowSize; i++)
+                for (int j = 0; j < columnSize; j++)
                     arr[i, j] = rnd.Next(10);
             return this;
         }
 
         public void Print() {
-            for (int i = 0; i < Row; i++) {
-                for (int j = 0; j < Column; j++)
+            for (int i = 0; i < rowSize; i++) {
+                for (int j = 0; j < columnSize; j++)
                     Console.Write(arr[i, j] + " ");
                 Console.WriteLine();
             }
         }
 
         public static Matrix operator *(Matrix a, Matrix b) {
-            Matrix result = new Matrix(a.Row, b.Column);
+            Matrix result = new Matrix(a.rowSize, b.columnSize);
             List<Thread> threads = new List<Thread>();
-            for (int i = 0; i < a.Row; i++)
-                for (int j = 0; j < b.Column; j++) {
+            for (int i = 0; i < a.rowSize; i++)
+                for (int j = 0; j < b.columnSize; j++) {
                     int tempi = i;
                     int tempj = j;
                     Thread thread = new Thread(() => VectorMult(tempi, tempj, a, b, result));
@@ -77,23 +97,5 @@ namespace MatrixMultiplication {
         }
     }
 
-    class Program {
-        static void tmp(string[] args) {
-            Console.Write("n=");
-            int n = int.Parse(Console.ReadLine());
-            Console.Write("m=");
-            int m = int.Parse(Console.ReadLine());
-            Console.Write("k=");
-            int k = int.Parse(Console.ReadLine());
-            Matrix A = new Matrix(n, m).RandomValues();
-            Matrix B = new Matrix(m, k).RandomValues();
-            A.Print();
-            Console.WriteLine(new String('-', 20));
-            B.Print();
-            Console.WriteLine(new String('-', 20));
-            Matrix C = A * B;
-            C.Print();
-            Console.ReadLine();
-        }
-    }
+
 }
